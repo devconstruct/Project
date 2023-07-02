544b25b1ba7cae3e75debaf0e6ba988028e7e66e
ARS <xxx@gmail.com>
2023-07-02 17:37:50 -0600

    estilos antes de guardar

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const path = require('path');
-const exphbs = require('express-handlebars');
+const { engine } = require('express-handlebars');
 
 //inicializamos la variable express
 const app = express();
@@ -9,14 +9,15 @@ const app = express();
 //Configuraciones generales
 app.set('port', process.env.PORT || 3000);
 //usaremos la carpeta views como principla de nuestros 
-exphbs.create(({
-    defaultLayout: 'main',
-    layoutsDir: path.join(app.get('views'), 'layouts'),
-    partialsDir: path.join(app.get('views'), 'partials'),
-    extname: '.hbs',
-    helpers: require('./lib/handlebars')
-  }))
-  app.set('view engine', '.hbs');
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', engine({
+  defaultLayout: 'main',
+  layoutsDir: path.join(app.get('views'), 'layouts'),
+  partialsDir: path.join(app.get('views'), 'partials'),
+  extname: '.hbs',
+  helpers: require('./lib/handlebars')
+}));
+app.set('view engine', '.hbs');
 
 //MIDDLEWARES
 app.use(morgan('dev'));
--- src/keys.js
+++ src/keys.js
@@ -2,6 +2,7 @@ module.exports = {
     database: {
         host: 'localhost',
         user: 'antonio',
+        port: '8889',
         password: '123456789',
         database: 'database_links'
     }
