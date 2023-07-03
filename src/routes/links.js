7adf0c1c85f48c7b1ca96c2893907ddad62b4197
ARS <xxx@gmail.com>
2023-07-02 18:50:12 -0600

    Mostrar datos de la BD

--- src/routes/links.js
+++ src/routes/links.js
@@ -3,10 +3,12 @@ const router = express.Router();
 
 const pool = require('../database');
 
+//Ruta donde se encuentra el formulario para insertar datos
 router.get('/add', (req, res) =>{
     res.render('links/add');
 });
 
+//Ruta el cual recibe el servidor para insertar los datos a la BD
 router.post('/add', async(req,res)=>{
     const { title, url, description } = req.body;
     const newLink = {
@@ -15,7 +17,13 @@ router.post('/add', async(req,res)=>{
         description
     };
     await pool.query('INSERT INTO links set ?', [newLink]);
-    res.send('recibido');
+    res.redirect('/links');
 });
+
+//Ruta que se usa para mostrar los datos que se encuentran en la BD
+router.get('/', async(req,res) =>{
+    const links = await pool.query('SELECT * FROM links');
+    res.render('links/list', { links });
+})
 
 module.exports = router;
